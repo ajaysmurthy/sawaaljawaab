051767d137c29e18fdea7e47225d78813aaec058
ajaysmurthy <ajays.murthy@upf.edu>
2015-10-20 14:52:01 +0200

    version start of tabla analysis module

---
+++ tabla.py
@@ -0,0 +1,22 @@
+## Table analysis and synthesis module for the HAMR 2015 ISMIR hack
+
+import os
+import essentia as es
+import numpy as np
+import pickle
+
+strokes = {'dha', }
+
+def buildStrokeModels():
+    
+
+def testModule(inputFile, pulsePos):
+    outFile = 'outTabla.wav'
+    
+    opulsePos = ipulsePos
+    return outFile, opulsePos
+
+if __name__ == "__main__":
+    outFile = testModule(inputFile = 'inTabla.wav',ipulsePos = None)
+    print "Stored output file to %s" %outFile
+    
